9c64bb88a6e7d9db1fd984f794251461bf9e2855
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-03-05 19:29:00 +0100

    Création initiale et test unitaire des composants de pages dans un dossier spécifiques aux pages de l'application (app/pages)

--- works/site/tta_angular/src/app/app.module.ts
+++ works/site/tta_angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArtisanListComponent } from './pages/artisan-list/artisan-list.component';
+import { ArtisanDetailComponent } from './pages/artisan-detail/artisan-detail.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtisanListComponent,
+    ArtisanDetailComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
