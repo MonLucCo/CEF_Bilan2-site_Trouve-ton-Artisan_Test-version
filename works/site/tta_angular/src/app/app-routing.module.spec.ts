d3956b929e6624c9e86c1c307b726f8ef538999b
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-03-07 11:51:10 +0100

    Création initiale et test unitaire du composant de routage (app-routing.module)

---
+++ works/site/tta_angular/src/app/app-routing.module.spec.ts
@@ -0,0 +1,55 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { provideRouter, ROUTES } from '@angular/router';
+import { Location } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { ArtisanListComponent } from './pages/artisan-list/artisan-list.component';
+import { ArtisanDetailComponent } from './pages/artisan-detail/artisan-detail.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { routes } from './app-routing.module';
+
+describe('AppRoutingModule', () => {
+    let harness: RouterTestingHarness;
+    let location: Location;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [
+                provideRouter(routes)
+            ]
+        }).compileComponents();
+
+        harness = await RouterTestingHarness.create();
+        location = TestBed.inject(Location);
+    });
+
+    it('should navigate to /accueil and render HomeComponent', async () => {
+        const fixture = await harness.navigateByUrl('/accueil', HomeComponent);
+        expect(fixture).not.toBeNull(); // Vérifiez que 'fixture' n'est pas null
+        expect(location.path()).toBe('/accueil');
+    });
+
+    it('should navigate to /liste-artisans and render ArtisanListComponent', async () => {
+        const fixture = await harness.navigateByUrl('/liste-artisans', ArtisanListComponent);
+        expect(fixture).not.toBeNull(); // Vérifiez que 'fixture' n'est pas null
+        expect(location.path()).toBe('/liste-artisans');
+    });
+
+    it('should navigate to /fiche-artisan and render ArtisanDetailComponent', async () => {
+        const fixture = await harness.navigateByUrl('/fiche-artisan', ArtisanDetailComponent);
+        expect(fixture).not.toBeNull(); // Vérifiez que 'fixture' n'est pas null
+        expect(location.path()).toBe('/fiche-artisan');
+    });
+
+    it('should navigate to / and render /accueil and HomeComponent', async () => {
+        const fixture = await harness.navigateByUrl('/', HomeComponent);
+        expect(fixture).not.toBeNull(); // Vérifiez que 'fixture' n'est pas null
+        expect(location.path()).toBe('/accueil');
+    });
+
+    it('should navigate to an unknown path and render /erreur-404 and Error404Component', async () => {
+        const fixture = await harness.navigateByUrl('/unknown-path', Error404Component);
+        expect(fixture).not.toBeNull(); // Vérifiez que 'fixture' n'est pas null
+        expect(location.path()).toBe('/erreur-404');
+    });
+});
