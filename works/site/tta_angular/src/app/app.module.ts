dfe42d30860c79e322fb0ae47a67ac2fd845169c
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2025-06-02 17:05:07 +0200

    Tests et validation - Assurer la qualité et la conformité du site - Partie 2b.  Réalisation d'un 'build' : - Rappatriement de tous les styles dans custom-bootstrap.scss (réduction liée au css). - utilisation bootstrap.esm.js pour améliorer le fonctionnement. - mise en commentaire des imports inutiles pour réduire la taille du code.

--- works/site/tta_angular/src/app/app.module.ts
+++ works/site/tta_angular/src/app/app.module.ts
@@ -7,36 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArtisansPageComponent } from './pages/artisans-page/artisans-page.component';
-import { ArtisanContactPageComponent } from './pages/artisan-contact-page/artisan-contact-page.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ArtisansListComponent } from './components/artisans-list/artisans-list.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { FilterByCategoryPipe } from './pipes/filter-by-category/filter-by-category.pipe';
-import { SearchPipe } from './pipes/search/search.pipe';
-import { TopArtisansPipe } from './pipes/top-artisans/top-artisans.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TopArtisansComponent } from './components/top-artisans/top-artisans.component';
-import { CategoryArtisansComponent } from './components/category-artisans/category-artisans.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { SearchArtisansComponent } from './components/search-artisans/search-artisans.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { ArtisanService } from './services/artisan/artisan.service';
 import { FicheArtisanComponent } from './components/fiche-artisan/fiche-artisan.component';
 import { TopFilterPipe } from './pipes/top-filter/top-filter.pipe';
-import { CategoryFilterPipe } from './pipes/category-filter/category-filter.pipe';
-import { SearchFilterPipe } from './pipes/search-filter/search-filter.pipe';
-import { IdFilterPipe } from './pipes/id-filter/id-filter.pipe';
 import { TopArtisanTestComponent } from './components/tests/top-artisan-test/top-artisan-test.component';
 import { RatingStarsComponent } from './components/rating-stars/rating-stars.component';
 import { ContactArtisanComponent } from './components/contact-artisan/contact-artisan.component';
 import { ApiMaildevTestComponent } from './components/tests/api-maildev-test/api-maildev-test.component';
 import { ListArtisansComponent } from './components/list-artisans/list-artisans.component';
 import { FindArtisanComponent } from './components/find-artisan/find-artisan.component';
-import { FindArtisanTestComponent } from './components/tests/find-artisan-test/find-artisan-test.component';
-import { TestHeaderComponent } from './components/tests/test-header/test-header.component';
-import { TestFooterComponent } from './components/tests/test-footer/test-footer.component';
 import { MentionsLegalesComponent } from './legals/mentions-legales/mentions-legales.component';
 import { DonneesPersonnellesComponent } from './legals/donnees-personnelles/donnees-personnelles.component';
 import { AccessibiliteComponent } from './legals/accessibilite/accessibilite.component';
@@ -47,40 +34,40 @@ import { GestionCookiesComponent } from './legals/gestion-cookies/gestion-cookie
 import { ConstructionComponent } from './legals/construction/construction.component';
 import { TitleComponent } from './components/title/title.component';
 import { firstValueFrom } from 'rxjs';
+// import { ArtisanContactPageComponent } from './pages/artisan-contact-page/artisan-contact-page.component';
+// import { ArtisansListComponent } from './components/artisans-list/artisans-list.component';
+// import { FilterByCategoryPipe } from './pipes/filter-by-category/filter-by-category.pipe';
+// import { SearchPipe } from './pipes/search/search.pipe';
+// import { TopArtisansPipe } from './pipes/top-artisans/top-artisans.pipe';
+// import { CategoryArtisansComponent } from './components/category-artisans/category-artisans.component';
+// import { SearchArtisansComponent } from './components/search-artisans/search-artisans.component';
+// import { CategoryFilterPipe } from './pipes/category-filter/category-filter.pipe';
+// import { SearchFilterPipe } from './pipes/search-filter/search-filter.pipe';
+// import { IdFilterPipe } from './pipes/id-filter/id-filter.pipe';
+// import { FindArtisanTestComponent } from './components/tests/find-artisan-test/find-artisan-test.component';
+// import { TestHeaderComponent } from './components/tests/test-header/test-header.component';
+// import { TestFooterComponent } from './components/tests/test-footer/test-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ArtisansPageComponent,
-    ArtisanContactPageComponent,
     Error404PageComponent,
     HeaderComponent,
     FooterComponent,
     ContactFormComponent,
-    FilterByCategoryPipe,
-    SearchPipe,
-    TopArtisansPipe,
-    ArtisansListComponent,
     TopArtisansComponent,
-    CategoryArtisansComponent,
     SearchBarComponent,
-    SearchArtisansComponent,
     ContactPageComponent,
     FicheArtisanComponent,
     TopFilterPipe,
-    CategoryFilterPipe,
-    SearchFilterPipe,
-    IdFilterPipe,
     TopArtisanTestComponent,
     RatingStarsComponent,
     ContactArtisanComponent,
     ApiMaildevTestComponent,
     ListArtisansComponent,
     FindArtisanComponent,
-    FindArtisanTestComponent,
-    TestHeaderComponent,
-    TestFooterComponent,
     MentionsLegalesComponent,
     DonneesPersonnellesComponent,
     AccessibiliteComponent,
@@ -89,7 +76,20 @@ import { firstValueFrom } from 'rxjs';
     PolitiqueCookiesComponent,
     GestionCookiesComponent,
     ConstructionComponent,
-    TitleComponent
+    TitleComponent,
+    // ArtisanContactPageComponent,
+    // FilterByCategoryPipe,
+    // SearchPipe,
+    // TopArtisansPipe,
+    // ArtisansListComponent,
+    // CategoryArtisansComponent,
+    // SearchArtisansComponent,
+    // CategoryFilterPipe,
+    // SearchFilterPipe,
+    // IdFilterPipe,
+    // FindArtisanTestComponent,
+    // TestHeaderComponent,
+    // TestFooterComponent,
   ],
   imports: [
     CommonModule,
